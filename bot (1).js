7a86c03bc4d46add8fd0306409bea0c4b24744a5
SuilMAN009 <44708799+SuilMAN009@users.noreply.github.com>
2018-11-04 00:06:49 +0400

    Update bot (1).js

--- bot (1).js
+++ bot (1).js
@@ -3,12 +3,12 @@ const client = new Discord.Client();
 var prefix = "+";
 client.on('ready', () => {
    console.log(`----------------`);
-      console.log(`Cyhper Bot Script By : DREAM`);
+      console.log(`Cyhper Bot Script By : IixRp_`);
         console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : IixRp_ ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setGame(`msh3'ol`,"http://twitch.tv/YouTube")
 client.user.setStatus("dnd")
 });
 
